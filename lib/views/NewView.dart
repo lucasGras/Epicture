55c5b74e36bbc927321b718826386fb227525a8f
Lucas GRAS <lucas.gras@face06.com>
2019-10-04 22:11:55 +0200

    [+] Create Camera view, search view. Update android gradle for camera sdk

--- lib/views/NewView.dart
+++ lib/views/NewView.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:path/path.dart' show join;
+import 'package:path_provider/path_provider.dart';
 
 class NewView extends StatefulWidget {
     NewView({Key key}) : super(key: key);
@@ -9,10 +12,32 @@ class NewView extends StatefulWidget {
 
 class _NewViewState extends State<NewView> {
 
+    CameraDescription camera;
+
+    void searchForCamera() async {
+        dynamic cameras = await availableCameras();
+        setState(() {
+            this.camera = cameras.first;
+        });
+    }
+
+    @override
+    void initState() {
+        super.initState();
+
+        this.searchForCamera();
+    }
+
     @override
     Widget build(BuildContext context) {
-        return Container(
-            child: Text("NEW PHOTO"),
-        );
+        if (this.camera == null) {
+            return Container(
+                child: Text("WAIT FOR PHOTO"),
+            );
+        } else {
+            return Container(
+                child: Text("NEW PHOTO"),
+            );
+        }
     }
 }
--- lib/views/SearchView.dart
+++ lib/views/SearchView.dart
@@ -12,7 +12,57 @@ class _SearchViewState extends State<SearchView> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("Search"),
+      child: Column(
+        children: <Widget>[
+          createSearchBar(context),
+          createCardGrid(context)
+        ],
+      ),
+    );
+  }
+
+  Widget createCardGrid(BuildContext context) {
+    return Flexible(
+      child: GridView.builder(
+          gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+          itemCount: 20,
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          itemBuilder: (BuildContext context, int index) {
+            return createResultCard(context);
+          }
+      )
+    );
+  }
+
+  Widget createResultCard(BuildContext context) {
+    return Container(
+      child: Card(
+        semanticContainer: true,
+        child: GridTile(
+          child: Container(
+            decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage('https://placeimg.com/640/480/any'))),
+          ),
+        )
+      ),
+    );
+  }
+
+  Widget createSearchBar(BuildContext context) {
+    return Container(
+      alignment: Alignment.topCenter,
+      child: TextField(
+          decoration: InputDecoration(
+              border: InputBorder.none,
+              prefixIcon: new Icon(Icons.search, color: Colors.grey),
+              hintText: "Search...",
+              hintStyle: new TextStyle(color: Colors.grey)
+          )
+      ),
     );
   }
 }
